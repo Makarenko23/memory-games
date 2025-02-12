03f2496626f3d74da30e2bb229e4763523acb626
Serhii Makarenko <s.makarenko2003@gmail.com>
2025-02-12 19:59:24 +0200

    after size changing during the game, start button is visible and modal result is hidden

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,53 @@
+const sizeTabs = document.querySelectorAll('.size__item'),
+      sizeTabActive = document.querySelector('.size--active'),
+      blockContainer = document.querySelector('.blocks');
+
+
+sizeTabs.forEach((sizeTab, i) => {
+    sizeTab.addEventListener('click', () =>{
+        sizeTabActive.style.transform = `translateX(calc(${i} * 100%))`;
+        changeColor(i);
+
+        let cells = (i === 0) ? 4 : (i === 1) ? 5 : 6;
+        let totalCells = cells * cells;
+
+        blockContainer.innerHTML = '';
+
+        for(let j = 0; j < totalCells; j++){
+            blockContainer.innerHTML += `<li class="block"></li>`;
+        }
+        blockContainer.style.width = `${cells * 125 + 50}px`;
+        blockContainer.style.height = `${cells * 125 + 50}px`;
+
+        // start.style.display = 'inline-block';
+        // start.textContent = "START";
+
+        renderGame();
+    })
+})
+
+function changeColor(i){
+    sizeTabs.forEach(sizeTab => {
+        sizeTab.style.color = "#000";
+    })
+    sizeTabs[i].style.color = "#185ee0";
+}
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
 function renderGame(){
 
     const blocks = document.querySelectorAll('.block'),
-    start = document.querySelector('.button'),
+    start = document.querySelectorAll('.button'),
     result = document.querySelector('.result'),
     scoreText = document.querySelector('.score'),
-    recordText = document.querySelector('.record');
+    recordText = document.querySelector('.record'),
+    stats = document.querySelector('.stats');
+
+    start[0].style.display = 'inline-block';
+    result.style.display = 'none';
+    stats.style.marginTop = '0px';
 
 let score = 0;
 let order = [];
@@ -75,58 +118,34 @@ function generateOrder(){
               scoreText.textContent = `Score: ${score}`;
               record = score;
               result.style.display = 'flex';
-              start.style.display = 'inline-block';
-              start.textContent = "RESTART";
+            //   start.style.display = 'inline-block';
+            //   start.textContent = "RESTART";
           }
       })
   })
 
 
-start.addEventListener('click', () => {
-  order = [];
-  score = 0;
-  scoreText.textContent = `Score: ${score}`;
-  generateOrder();
-  start.style.display = 'none';
-  result.style.display = 'none';
-})
+  start.forEach((startB, i) => {
+    startB.addEventListener('click', () => {
+        order = [];
+        score = 0;
+        scoreText.textContent = `Score: ${score}`;
+        generateOrder();
+        start[0].style.display = 'none';
+        result.style.display = 'none';
+        stats.style.marginTop = '100px';
+      })
+  }) 
+
+// start.addEventListener('click', () => {
+//   order = [];
+//   score = 0;
+//   scoreText.textContent = `Score: ${score}`;
+//   generateOrder();
+//   start.style.display = 'none';
+//   result.style.display = 'none';
+//   stats.style.marginTop = '100px';
+// })
 }
 
 renderGame();
-//////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////
-
-const sizeTabs = document.querySelectorAll('.size__item'),
-      sizeTabActive = document.querySelector('.size--active');
-
-const blockContainer = document.querySelector('.blocks');
-
-sizeTabs.forEach((sizeTab, i) => {
-    sizeTab.addEventListener('click', () =>{
-        sizeTabActive.style.transform = `translateX(calc(${i} * 100%))`;
-        changeColor(i);
-
-        let cells = (i === 0) ? 4 : (i === 1) ? 5 : 6;
-        let totalCells = cells * cells;
-
-        blockContainer.innerHTML = '';
-
-        for(let j = 0; j < totalCells; j++){
-            blockContainer.innerHTML += `<li class="block"></li>`;
-        }
-        blockContainer.style.width = `${cells * 125 + 50}px`;
-        blockContainer.style.height = `${cells * 125 + 50}px`;
-
-        // start.style.display = 'inline-block';
-        // start.textContent = "START";
-
-        renderGame();
-    })
-})
-
-function changeColor(i){
-    sizeTabs.forEach(sizeTab => {
-        sizeTab.style.color = "#000";
-    })
-    sizeTabs[i].style.color = "#185ee0";
-}
